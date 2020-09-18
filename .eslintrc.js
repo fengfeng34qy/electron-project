632e1fd6ad8be0d2a4754806c58499719ca2fcde
fengfeng34qy <405010021@qq.com>
2020-09-18 22:47:48 +0800

    初始化仓库

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,23 @@ module.exports = {
   ],
   'rules': {
     // allow paren-less arrow functions
-    'arrow-parens': 0,
+    // 'arrow-parens': 0,
     // allow async-await
-    'generator-star-spacing': 0,
+    // 'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+
+    "semi": [2, "always"],
+    "quotes": [0, "single", "avoid-escape"],
+    "space-before-function-paren": [0, "never"],
+    // 因为agree_sdk中会出现
+    "wrap-iife": [0, "any"],
+    "spaced-comment": [0, "always", { "markers": ["global", "globals", "eslint", "eslint-disable", "*package", "!", ","] }],
+    "eqeqeq": [0, "allow-null"],
+    "one-var": [0, { "initialized": "never" }],
+    "eol-last": 0,
+    "no-multi-spaces": 0,
+    "no-useless-return": 0,
+    "parser": "babel-eslint",
   }
 }
